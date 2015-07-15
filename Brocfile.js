80c84b6571deb2a044a5f7855e1d1574a29faac9
Coleman Cox <colezane82@gmail.com>
2015-07-15 15:13:26 -0500

    Set up sass compile

--- Brocfile.js
+++ Brocfile.js
@@ -1,1 +1,14 @@
-module.exports = 'public';
+module.exports = merge (['public', compiledCss]);
+var merge = require('broccoli-merge-trees');
+var sassCompile = require ('broccoli-sass');
+var compiledCSS = sassCompile(includePaths, 'app.scss', 'app.css');
+
+var includePaths = [
+   'assets',
+   'bower_components/reset-css',
+   'bower_components/fontawesome/scss',
+   'bower_components/bourbon/app/assets/stylesheets',
+   'bower_components/neat/app/assets/stylesheets'
+
+
+];
