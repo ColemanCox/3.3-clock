ef92fbed39ff7723384923d8c501a6f787b02595
Coleman Cox <colezane82@gmail.com>
2015-07-15 21:35:31 -0500

    I think I'm at least in the same Area Code .....I hope

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-new Date();
-new Date(value);
-new Date(dateString);
-new Date( hour[, minutes[, seconds[, milliseconds]]]]);
+//var or function not 100% yet gonna read yo notes
+var min = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+       for(i = 9; i > 1;i++); {
+
+
+     return  min[i++];
+ }
+//I think I'm getting warm
+
+
+
+
+//window.setTimeout(function () {    just commentn out so
+  //  I dont forget it if I need it.
+  //  .innerHTML = ""
+//}
