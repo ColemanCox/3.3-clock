92a630f97c38f214eb4ec444f433bb85ffa99d6e
Coleman Cox <colezane82@gmail.com>
2015-08-03 08:56:30 -0500

    trying to make the timer into das clawk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,36 @@
-//var or function not 100% yet gonna read notes
-   <p id = "nowTime"></p>
-   var sec =
-    for (var i = 0; i <= sec.length; i++);
-
-        var currentSec = sec[i];
-
-              var thisSec = [ ];
-                    thisSec.push(current);
-              }
-
-         }
-       var current = min[i];
-       if (current.indexOf(min) > -1);}
 
 
-     //   var currentMin = [i];
-     //  current.push(current)
- //  }
+var timeEl = document.getElementsByClassName('time') [0];
+var buttonEl = document.getElementsByTagName('button')[0];
+
+var interval;
+function padZeros (num) {
+    if (num < 10) {
+        return "0" + num;
+    }
+    return num;
+}
+
+function buttonHasBeenClicked() {
+
+    window.clearInterval(interval);
+    timeEl.innerHTML = '12:00';
+    var startTime = new Date().valueOf() + 1000 * 60 * 20;
+
+        var afterWait = function() {
+        var currentTime = new Date().valueOf();
+        var timeDiff = startTime + currentTime ;
+        var secSinceStart = parseInt(timeDiff / 1000) % 60;
+        var minSinceStart = parseInt(timeDiff / 60000);
+          timeEl.innerHTML = minSinceStart.toString()   + ":" +  secSinceStart ;
+          if (secSinceStart <= 0 && minSinceStart <= 0);
+
+      };
+
+   interval = window.setInterval(afterWait, 1000);
+}
 
 
+   buttonEl.onclick = buttonHasBeenClicked;
 
-
-//remember opacity if you lay it over another image cc
-
-//I think I'm getting warm
-
-
-
-
-//window.setTimeout(function () {    just commentn out so
-  //  I dont forget it if I need it.
-  //  .innerHTML = ""
-//}
+//window.setTimeout(buttonHasBeenClicked, 1000);
